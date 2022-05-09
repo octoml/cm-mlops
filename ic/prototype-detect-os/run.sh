c32cea5906f158f946ec2269933cf46ce051de58
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-05-09 12:32:34 +0200

    prototyping IC

--- ic/prototype-detect-os/run.sh
+++ ic/prototype-detect-os/run.sh
@@ -1,2 +1,5 @@
 uname -m > tmp-run.out
 uname -a >> tmp-run.out
+
+echo "CM_HOST_OS_TEST=$pwd" > tmp-run-env.out
+echo "CM_HOST_OS_VER=`uname -r`" >> tmp-run-env.out
--- ic/prototype-echo-hello-world-py/run.sh
+++ ic/prototype-echo-hello-world-py/run.sh
@@ -1,1 +1,3 @@
 python3 ${CM_CURRENT_IC_PATH}/code.py
+
+echo "CM_NEW_VAR_FROM_RUN=$MLPERF_XYZ" > tmp-run-env.out
