969560e766c350e86f8f9b38fe56ff302c868af4
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-05-11 16:15:48 +0200

    clean up on Linux

--- ic/prototype-get-imagenet-val/run.sh
+++ ic/prototype-get-imagenet-val/run.sh
@@ -1,6 +1,11 @@
 wget -nc https://www.dropbox.com/s/57s11df6pts3z69/ILSVRC2012_img_val_500.tar --no-check-certificate
+test $? -eq 0 || exit 1
+
 mkdir images
+
 tar -C images -xvf ILSVRC2012_img_val_500.tar
+test $? -eq 0 || exit 1
+
 rm -rf ILSVRC2012_img_val_500.tar
 
 echo "CM_DATASET_PATH=$PWD/images" > tmp-run-env.out
--- ic/prototype-image-classification-onnx-py/run.bat
+++ ic/prototype-image-classification-onnx-py/run.bat
@@ -1,5 +1,6 @@
 echo %CM_PYTHON_BIN%
 echo %CM_DATASET_PATH%
+echo %CM_DATASET_AUX_PATH%
 echo %CM_ML_MODEL_FILE_WITH_PATH%
 
 rem python src/onnx_classify.py %*
