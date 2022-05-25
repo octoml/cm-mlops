b8104fa4ecea5a0e9bf4773abfaa1a8c83eb459a
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-05-25 17:50:11 +0100

    Added run-macOS.sh for get-sys-utils

---
+++ ic/prototype-get-sys-utils-cm/run-macOS.sh
@@ -0,0 +1,18 @@
+brew update && \
+     brew install \
+           git \
+           wget \
+           curl \
+           zip \
+           unzip \
+           bzip2 \
+           vim \
+           mc \
+           tree \
+           gcc \
+           autoconf \
+           autogen \
+           libtool \
+           make \
+           cmake \
+           python3
