531b1d23ba8d424da0a5302c9833ccc4cca2c6c0
Arjun <arjunsuresh1987@gmail.com>
2022-05-27 12:59:33 +0100

    Added env variables for cpuinfo

--- ic/prototype-get-cpu-info/customize.py
+++ ic/prototype-get-cpu-info/customize.py
@@ -28,7 +28,11 @@ def postprocess(i):
     new_state['cpu_info_raw'] = ss
 
     print ('')
-    for s in ss.split('\n'):
-        print (s)
-
+    if new_env['CM_HOST_OS_TYPE'] == 'linux':
+        vkeys = [ 'Architecture', 'Model name', 'Vendor ID', 'CPU family', 'NUMA node(s)', 'CPU(s)', 'On-line CPU(s) list', 'Socket(s)', 'Thread(s) per core' ]
+        for s in ss.split('\n'):
+            v = s.split(':')
+            key = v[0]
+            if key in vkeys:
+                new_env['CM_CPUINFO_'+key] = v[1].strip()
     return {'return':0}
--- ic/prototype-get-cpu-info/run.sh
+++ ic/prototype-get-cpu-info/run.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 if [[ ${CM_HOST_OS_FLAVOR} == "macos" ]]; then
     sysctl -a | grep machdep.cpu > tmp-lscpu.out
 else
-	echo  ${CM_HOST_OS_FLAVOR} 
     lscpu > tmp-lscpu.out
 fi
