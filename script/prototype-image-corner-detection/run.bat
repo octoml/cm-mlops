c2287e9d5487f3fe5c2d610b43fe8abc94702e22
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-06-06 13:45:08 +0200

    fixing input/output in corner detection

--- script/prototype-image-corner-detection/run.bat
+++ script/prototype-image-corner-detection/run.bat
@@ -1,11 +1,27 @@
+rem Compile
+
+echo.
+echo Compiling program ...
+echo.
+
 cd %CM_CURRENT_SCRIPT_PATH%
 
-%CM_C_COMPILER_WITH_PATH% -O1 susan.c
+%CM_C_COMPILER_WITH_PATH% -O3 susan.c
+IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
-a.exe data.pgm data_edges.pgm -c
+rem Return to the original path obtained in CM
+
+echo.
+echo Running program ...
+echo.
 
 cd %CM_CURRENT_PATH%
 
+IF NOT DEFINED CM_INPUT SET CM_INPUT=%CM_CURRENT_SCRIPT_PATH%\data.pgm
+IF NOT DEFINED CM_OUTPUT SET CM_OUTPUT=output_image_with_edges.pgm
+
+%CM_CURRENT_SCRIPT_PATH%\a.exe %CM_INPUT% %CM_OUTPUT% -c
+IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 rem -c
 rem -e
