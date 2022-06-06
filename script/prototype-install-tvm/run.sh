b1a974a0f26373b5deb40e52a7faaf0695cd4ad8
Arjun <arjunsuresh1987@gmail.com>
2022-06-06 20:21:44 +0100

    Added LLVM and OPENMP variations for TVM

--- script/prototype-install-tvm/run.sh
+++ script/prototype-install-tvm/run.sh
@@ -10,7 +10,13 @@ cp cmake/config.cmake build
 
 cd build
 
-sed -i.bak 's/set(USE_LLVM OFF)/set(USE_LLVM ON)/' config.cmake
+if [[ ${CM_TVM_USE_LLVM} == "yes" ]]; then
+    sed -i.bak 's/set(USE_LLVM OFF)/set(USE_LLVM ON)/' config.cmake
+fi
+
+if [[ ${CM_TVM_USE_OPENMP} == "yes" ]]; then
+    sed -i.bak 's/set(USE_OPENMP none)/set(USE_OPENMP gnu)/' config.cmake
+fi
 
 cmake ..
 test $? -eq 0 || exit 1
