17405833665bc1e93820f9ff76deb28a0f543bdb
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-06-07 18:13:29 +0200

    fixed LLVM build from src

--- script/prototype-install-llvm-src/run.sh
+++ script/prototype-install-llvm-src/run.sh
@@ -19,14 +19,14 @@ echo "******************************************************"
 
 cd build
 
-#cmake \
-#    -DLLVM_ENABLE_PROJECTS=clang \
-#    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-#    -DCMAKE_BUILD_TYPE=Release \
-#    -DLLVM_ENABLE_RTTI=ON \
-#    -DLLVM_INSTALL_UTILS=ON \
-#    ../llvm/llvm
-#if [ "${?}" != "0" ]; then exit 1; fi
+cmake \
+    -DLLVM_ENABLE_PROJECTS=clang \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_INSTALL_UTILS=ON \
+    ../llvm/llvm
+if [ "${?}" != "0" ]; then exit 1; fi
 
 echo "******************************************************"
 CM_MAKE_CORES=${CM_MAKE_CORES:-${CM_HOST_CPU_NUMBER_OF_PROCESSORS}}
