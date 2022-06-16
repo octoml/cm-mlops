f187e3ada6b7b6cfc21001ed0538bfff72d9fad5
Arjun <arjunsuresh1987@gmail.com>
2022-06-16 21:36:57 +0100

    Added install-python-src script

---
+++ script/install-python-src/customize.py
@@ -0,0 +1,34 @@
+from cmind import utils
+import os
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    if os_info['platform'] == 'windows':
+        return {'return':1, 'error': 'Windows is not supported in this script yet'}
+
+    env = i['env']
+
+    automation = i['automation']
+
+    recursion_spaces = i['recursion_spaces']
+
+    need_version = env.get('CM_NEED_VERSION','')
+    if need_version == '':
+        return {'return':1, 'error':'internal problem - CM_NEED_VERSION is not defined in env'}
+
+    print (recursion_spaces + '    # Requested version: {}'.format(need_version))
+
+    return {'return':0}
+
+def postprocess(i):
+
+    env = i['env']
+
+    env['+PATH'] = [os.path.join(os.getcwd(), 'install', 'bin')]
+    env['+LD_LIBRARY_PATH'] = [os.path.join(os.getcwd(), 'install', 'lib')]
+    env['+C_INCLUDE_PATH'] = [os.path.join(os.getcwd(), 'install', 'include')]
+
+    return {'return':0}
+
